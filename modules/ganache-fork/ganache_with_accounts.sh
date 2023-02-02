6e88859ab4bbad052cd9a305eb21a47cb16aa8db
jaensen <daniel@circles.name>
2023-02-02 02:38:30 +0100

    configure ganache to use a fork

--- modules/ganache-fork/ganache_with_accounts.sh
+++ modules/ganache-fork/ganache_with_accounts.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-cp -r -f app/ganache-seed-db/* /var/ganache-db/*
-rm -r -f app/ganache-seed-db/
-
 node /app/dist/node/cli.js \
   --logging.debug=true \
   -v \
   --db="/var/ganache-db" \
-  --blockTime=1 \
-  --gasLimit 0xfffffffffffff \
+  --fork=https://rpc.circlesubi.id \
   --account="0x5bc6328efff9fc724aad89edf1356a6ba7bee56368b4b9b47b1f29a5cd6d73c7,1000000000000000000000000" \
   --account="0x89e62e74143e15eaba362a67f8d71e5371d1268e1769b2613b8483024d17e110,1000000000000000000000000" \
   --account="0xd0f7cc8f8e7d9e10fbf51d8ac39f390acf52bfa96224fb5cff6b189c1a68f328,1000000000000000000000000" \
