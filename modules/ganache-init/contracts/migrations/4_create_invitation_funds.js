ee8640383e789fcad19c04ff1945b6f8a4b49768
jaensen <4954577+jaensen@users.noreply.github.com>
2023-02-04 05:30:23 +0100

    Added missing 'setup' data for the invitation funds safe. Added API_SERVER_S3_* varaibles

--- modules/ganache-init/contracts/migrations/4_create_invitation_funds.js
+++ modules/ganache-init/contracts/migrations/4_create_invitation_funds.js
@@ -1,21 +1,44 @@
 const Web3 = require("web3");
-const proxyArtifacts = require('/app/contracts/build/contracts/GnosisSafeProxyFactory.json');
+const proxyFactoryArtifacts = require('/app/contracts/build/contracts/GnosisSafeProxyFactory.json');
+const safeArtifacts = require('/app/contracts/build/contracts/GnosisSafe.json');
 
 const web3Provider = new Web3.providers.HttpProvider('http://ganache:8545');
 const web3Instance = new Web3(web3Provider);
 const account = web3Instance.eth.accounts.privateKeyToAccount("0x5bc6328efff9fc724aad89edf1356a6ba7bee56368b4b9b47b1f29a5cd6d73c7");
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
 async function createSafe() {
     const safeMasterCopyAddress = "0x5e484da6227ab3ba047121742ee766cc6389db4f";
     const safeProxyFactoryAddress = "0xD5E7d393226A6e9089601080E4b92BcaF536BaDA";
-    const proxyFactoryAbi = proxyArtifacts.abi;
+
+    const proxyFactoryAbi = proxyFactoryArtifacts.abi;
     const proxyFactoryContractInstance = new web3Instance.eth.Contract(proxyFactoryAbi, safeProxyFactoryAddress);
-    const createProxyCallData = proxyFactoryContractInstance.methods.createProxy(safeMasterCopyAddress, "0x").encodeABI();
+
+    const safeAbi = safeArtifacts.abi;
+    const safeContractInstance = new web3Instance.eth.Contract(safeAbi, safeMasterCopyAddress);
+
+    const proxySetupData = safeContractInstance.methods
+        .setup(
+            [account.address],
+            1, // threshold (how many owners are required to sign a transaction -> 1)
+            ZERO_ADDRESS, // delegatecall for modules (none)
+            "0x", // init data for modules (none)
+            "0xf48f2b2d2a534e402487b3ee7c18c33aec0fe5e4", // fallbackHandler
+            ZERO_ADDRESS, // paymentToken (none defaults to ETH)
+            0, // payment
+            ZERO_ADDRESS // paymentReceiver
+        )
+        .encodeABI();
+
+
+    const createProxyData = proxyFactoryContractInstance.methods
+        .createProxy(safeMasterCopyAddress, proxySetupData)
+        .encodeABI();
 
     const signedTx = await account.signTransaction({
         from: account.address,
         to: safeProxyFactoryAddress,
-        data: createProxyCallData,
+        data: createProxyData,
         gas: 1000000
     });
 
@@ -23,7 +46,7 @@ async function createSafe() {
 
     let proxyAddress = "";
     for (let logEntry of createProxyTx.logs) {
-        if (logEntry.address?.toLowerCase() != safeProxyFactoryAddress.toLowerCase()) {
+        if (logEntry.address?.toLowerCase() !== safeProxyFactoryAddress.toLowerCase()) {
             continue;
         }
 
