2ab0ecfb35fc7adb9a6c12f08bcfe6be2ae4f56e
jaensen <daniel.janz@gnosis.pm>
2023-03-14 04:43:14 +0100

    delete certs only when all containers are shut down

--- reset.sh
+++ reset.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./remove-ca.sh && (cd modes/from-source && docker compose kill && docker compose down && sudo rm -r -f .state && docker compose build)
+(cd modes/from-source && docker compose kill && docker compose down && sudo rm -r -f .state && docker compose build) && ./remove-ca.sh
