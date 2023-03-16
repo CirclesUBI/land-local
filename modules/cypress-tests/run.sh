34ca1ebeee331404817918b25f79617f4ccc0c96
Ingo Collatz <incollatz@mail.de>
2023-03-17 01:56:05 +0200

    --use-file-for-fake-video-capture

--- modules/cypress-tests/run.sh
+++ modules/cypress-tests/run.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 cd /o-platform-cypress
-# curl -I -o /o-platform-cypress/cypress/fixtures/Person_1.y4m https://static.cirlcesubi.localhost/Person_1.y4m
-ls -l /o-platform-cypress/cypress/fixtures
-# Check if the above directory is empty
-if [ "$(ls -A /o-platform-cypress/cypress/fixtures)" ]; then
-   echo "Not empty"
-else
-   echo "Empty"
-fi
-npx cypress run
+npx cypress run --browser chromium
