19aae43b1ac3dd98c0ffa10f1d087303f696d67b
jaensen <4954577+jaensen@users.noreply.github.com>
2023-03-16 13:41:25 +0100

    run cypress

--- modules/cypress-tests/run.sh
+++ modules/cypress-tests/run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-cd /cypress-tests
-npm install -g npm@9.6.1
-npm install cypress --save-dev
-cypress install
+cd /o-platform-cypress
+mkdir -p /fixtures
+curl -o /fixtures/Person_1.y4m https://static.cirlcesubi.localhost/Person_1.y4m
 npx cypress run
