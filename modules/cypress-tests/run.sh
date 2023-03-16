eaeec79eb9af9c46cf2c076878f1497106f53b09
jaensen <4954577+jaensen@users.noreply.github.com>
2023-03-16 19:37:02 +0100

    changed path to invite qr code video

--- modules/cypress-tests/run.sh
+++ modules/cypress-tests/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /o-platform-cypress
-mkdir -p /fixtures
-curl -o /fixtures/Person_1.y4m https://static.cirlcesubi.localhost/Person_1.y4m
+mkdir -p /o-platform-cypress/cypress/fixtures
+curl -o /o-platform-cypress/cypress/fixtures/Person_1.y4m https://static.cirlcesubi.localhost/Person_1.y4m
 npx cypress run
