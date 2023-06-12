23efda82bb78b1bdf9d0af81164a9f90ba7c3b11
jon r <jon@gnosis.io>
2023-06-12 22:40:52 +0200

    APP-367: fix(start): separate startup stages for debugging

--- start.sh
+++ start.sh
@@ -1,2 +1,44 @@
 #!/bin/bash
-./clone-repos.sh && ./install-ca.sh && (cd modes/from-source && docker compose up -d)
+
+trap "echo; exit" INT
+trap "echo; exit" TERM
+
+clone-repositories () {
+    echo Clone repositories
+    ./clone-repos.sh
+    }
+
+install-certificate-authority () {
+    echo Install certificate authority
+    ./install-ca.sh
+    }
+
+build-container-images () {
+    echo Build container images from source
+    cd modes/from-source
+    set -x
+    docker compose build
+    }
+
+pull-container-images () {
+    echo Pull remaining container images
+    cd modes/from-source
+    set -x
+    docker compose pull
+    }
+
+start-compose-project () {
+    echo Start the project
+    cd modes/from-source
+    set -x
+    docker compose up -d
+    }
+
+echo "Check and prepare prerequisites"
+clone-repositories
+install-certificate-authority
+
+echo "Prepare and enter application runtime mode"
+build-container-images
+pull-container-images
+start-compose-project
