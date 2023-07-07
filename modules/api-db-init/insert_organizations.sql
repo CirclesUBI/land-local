256449a7c055c81bfbc9e764b27afd60b2bc9674
Thorsten Schau <thorsten@codeho.org>
2023-07-07 13:01:08 +0200

    clean up orga seed

--- modules/api-db-init/insert_organizations.sql
+++ modules/api-db-init/insert_organizations.sql
@@ -14,8 +14,8 @@ BEGIN
             p."circlesAddress" = lower(add_seed_org.safe_address)) THEN
     RETURN -1;
 END IF;
-INSERT INTO public."Profile"(status, "circlesAddress", "circlesSafeOwner", "firstName", "lastName", "avatarUrl", "avatarMimeType", "verifySafeChallenge", "newSafeAddress", "lastUpdateAt", "lastAcknowledged", "displayTimeCircles", type, "lastInvoiceNo", "lastRefundNo", "displayCurrency", "invoiceNoPrefix", "refundNoPrefix", "successorOfCirclesAddress", "createdAt", "verifyEmailChallenge", "askedForEmailAddress", "currentSimplePickupCodeRound", "lastSimplePickupCode", "largeBannerUrl", "smallBannerUrl", "productListingType", "inviteTriggerId", "confirmedLegalAge", age, gender, location, "businessCategoryId", "businessHoursFriday", "businessHoursMonday", "businessHoursSaturday", "businessHoursSunday", "businessHoursThursday", "businessHoursTuesday", "businessHoursWednesday", "phoneNumber", lat, lon, "locationName", "canInvite", "surveyDataSessionId", "shopEnabled")
-    VALUES ('', lower(safe_address), lower(safe_owner_address), user_name, NULL, 'https://picsum.photos/200?random=' || lower(safe_owner_address), NULL, NULL, NULL, now(), now(), TRUE, 'ORGANISATION', 0, 0, 'EURS', 'I-', NULL, NULL, now(), NULL, TRUE, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, FALSE, NULL, TRUE)
+INSERT INTO public."Profile"(status, "circlesAddress", "circlesSafeOwner", "firstName", "avatarUrl", "lastUpdateAt", "lastAcknowledged", "displayTimeCircles", type, "lastInvoiceNo", "lastRefundNo", "displayCurrency", "invoiceNoPrefix", "createdAt", "verifyEmailChallenge", "canInvite", "shopEnabled")
+    VALUES ('', lower(safe_address), lower(safe_owner_address), user_name, 'https://picsum.photos/200?random=' || lower(safe_owner_address), now(), now(), TRUE, 'ORGANISATION', 0, 0, 'EURS', 'I-', now(), TRUE, FALSE, TRUE)
 RETURNING
     id INTO profile_id;
     RETURN profile_id;
